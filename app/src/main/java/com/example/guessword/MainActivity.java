f77c0cc045af26227c3f498c342bbcfa840f0f43
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-11-25 07:22:26 +0200

    Added letter buttons

--- app/src/main/java/com/example/guessword/MainActivity.java
+++ app/src/main/java/com/example/guessword/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.guessword;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.guess_word);
+
+        // letter buttons
+        Button btnA = (Button)findViewById(R.id.button12);
+
     }
 }
