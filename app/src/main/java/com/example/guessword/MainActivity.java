a1b6409a551bb049fd3880f47cf6180dd5f43424
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-11-25 07:28:26 +0200

    Get letter buttons

--- app/src/main/java/com/example/guessword/MainActivity.java
+++ app/src/main/java/com/example/guessword/MainActivity.java
@@ -14,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
 
         // letter buttons
         Button btnA = (Button)findViewById(R.id.button12);
+        Button btnB = (Button)findViewById(R.id.button11);
+        Button btnC = (Button)findViewById(R.id.button10);
+        Button btnD = (Button)findViewById(R.id.button9);
+        Button btnE = (Button)findViewById(R.id.button8);
+        Button btnF = (Button)findViewById(R.id.button17);
+        Button btnG = (Button)findViewById(R.id.button16);
+        Button btnH = (Button)findViewById(R.id.button15);
+        Button btnI = (Button)findViewById(R.id.button14);
+        Button btnJ = (Button)findViewById(R.id.button13);
 
     }
 }
